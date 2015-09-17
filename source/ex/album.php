57f0380c7eb41a6bd07e821bf58b041b243c0705
Priyam Nigam <priyam.nigam@gmail.com>
2015-09-17 11:14:48 +0530

    atulchu
    atulchu

--- source/ex/album.php
+++ source/ex/album.php
@@ -5,6 +5,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "project";
+atul chu
 //atul
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
